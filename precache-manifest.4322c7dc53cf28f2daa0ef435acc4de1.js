f6026ed89e27be2dcc173bf70bca765533c9a96c
Ritu <ritu.melroy@gmail.com>
2020-06-09 23:03:27 +0530

    Updates

---
+++ precache-manifest.4322c7dc53cf28f2daa0ef435acc4de1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24fb597bb4684d5b8e53ca579d39f36",
+    "url": "/deploy-food-react/index.html"
+  },
+  {
+    "revision": "7772f032446f59cf6b56",
+    "url": "/deploy-food-react/static/css/main.f81fe396.chunk.css"
+  },
+  {
+    "revision": "aff6f8d8aa8827709f8b",
+    "url": "/deploy-food-react/static/js/2.645e5947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-food-react/static/js/2.645e5947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7772f032446f59cf6b56",
+    "url": "/deploy-food-react/static/js/main.48683b49.chunk.js"
+  },
+  {
+    "revision": "72d7b37688b4a7d3e307",
+    "url": "/deploy-food-react/static/js/runtime-main.4e832e21.js"
+  },
+  {
+    "revision": "94e3d47711b819cc3893cefe812fe430",
+    "url": "/deploy-food-react/static/media/kyoto-bar.94e3d477.jpeg"
+  },
+  {
+    "revision": "ccd35cfc30dbcd13b9f51612fdf0821e",
+    "url": "/deploy-food-react/static/media/kyoto-mobile.ccd35cfc.jpeg"
+  }
+]);
